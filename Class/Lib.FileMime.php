e381656c4ad87e047f3d98bcbaef4180b3a86136
eric <eric>
2006-03-30 16:21:50 +0000

    mime

--- Class/Lib.FileMime.php
+++ Class/Lib.FileMime.php
@@ -1,5 +1,8 @@
 <?php
-$mimeIcon=array(
+
+
+function getIconMimeArray() {
+  return array(
 		"application/msword" => "mime-wordprocessing",
 		"application/ogg" => "audio-x-generic",
 		"application/pdf" => "mime-pdf",
@@ -11,26 +14,28 @@ $mimeIcon=array(
 		"application/vnd.oasis.opendocument.presentation" => "mime-presentation",
 		"application/vnd.oasis.opendocument.spreadsheet" => "mime-spreadsheet",
 		"application/vnd.oasis.opendocument.text" => "mime-wordprocessing",
+		"application/vnd.oasis.opendocument.draw" => "mime-vectorgfx",
 		"application/vnd.stardivision.calc" => "mime-spreadsheet",
 		"application/vnd.stardivision.impress" => "mime-presentation",
 		"application/vnd.stardivision.writer" => "mime-wordprocessing",
 		"application/vnd.sun.xml.calc" => "mime-spreadsheet",
 		"application/vnd.sun.xml.impress" => "mime-presentation",
 		"application/vnd.sun.xml.writer" => "mime-wordprocessing",
+		"application/vnd.sun.xml.draw" => "mime-vectorgfx",
 		"application/wordperfect" => "mime-wordprocessing",
-		"application/x-7z-compressed" => "package-x-generic",
+		"application/x-7z-compressed" => "mime-tgz",
 		"application/x-abiword" => "mime-wordprocessing",
 		"application/x-applix-spreadsheet" => "mime-spreadsheet",
 		"application/x-applix-word" => "mime-wordprocessing",
-		"application/x-archive" => "package-x-generic",
-		"application/x-arj" => "package-x-generic",
-		"application/x-bzip-compressed-tar" => "package-x-generic",
-		"application/x-bzip" => "package-x-generic",
-		"application/x-compressed-tar" => "package-x-generic",
-		"application/x-compress" => "package-x-generic",
-		"application/x-cpio-compressed" => "package-x-generic",
-		"application/x-cpio" => "package-x-generic",
-		"application/x-deb" => "package-x-generic",
+		"application/x-archive" => "mime-tar",
+		"application/x-arj" => "mime-tgz",
+		"application/x-bzip-compressed-tar" => "mime-tgz",
+		"application/x-bzip" => "mime-tgz",
+		"application/x-compressed-tar" => "mime-tgz",
+		"application/x-compress" => "mime-tgz",
+		"application/x-cpio-compressed" => "mime-tgz",
+		"application/x-cpio" => "mime-tgz",
+		"application/x-deb" => "mime-tgz",
 		"application/x-dvi" => "mime-wordprocessing",
 		"application/x-executable" => "application/x-executable",
 		"application/x-font-afm" => "font-x-generic",
@@ -40,7 +45,7 @@ $mimeIcon=array(
 		"application/x-font-sunos-news" => "font-x-generic",
 		"application/x-font-ttf" => "font-x-generic",
 		"application/x-gnumeric" => "mime-spreadsheet",
-		"application/x-gzip" => "package-x-generic",
+		"application/x-gzip" => "mime-tgz",
 		"application/xhtml+xml" => "text-html",
 		"application/x-killustrator" => "image-x-generic",
 		"application/x-kpresenter" => "mime-presentation",
@@ -50,7 +55,7 @@ $mimeIcon=array(
 		"application/x-perl" => "mime-shellscript",
 		"application/x-php" => "text-html",
 		"application/x-python-bytecode" => "mime-shellscript",
-		"application/x-rar" => "package-x-generic",
+		"application/x-rar" => "mime-tgz",
 		"application/x-rpm" => "mime-rpm",
 		"application/x-shellscript" => "mime-shellscript",
 		"application/x-shockwave-flash" => "video-x-generic",
@@ -60,20 +65,93 @@ $mimeIcon=array(
 		"application/x-zip" => "mime-zip",
 		"application/zip" => "mime-zip",
 		"application" => "mime-zip",
-		"audio" => "audio-x-generic",
+		"audio" => "mime-sound",
 		"image" => "mime-image2",
+		"video" => "mime-video",
 		"image/jpeg" => "mime-image",
-		"text-html" => "text-html",
+		"text/html" => "mime-html",
+		"text/xml" => "mime-xml",
+		"text/x-c++" => "mime-source_cpp",
+		"text/x-c" => "mime-source_c",
+		"text/x-c-header" => "mime-source_h",
+		"text/x-php" => "mime-source_php",
 		"text" => "mime-txt",
-		"text-vnd.wap.wml" => "text-html",
-		"text-x-csh" => "mime-shellscript",
-		"text-x-python" => "mime-shellscript",
-		"text-x-sh" => "mime-shellscript",
-		"text-x-vcalendar" => "mime-vcalendar",
-		"text-x-vcard" => "mime-address-book",
-		"text-x-zsh" => "mime-shellscript",
+		"text/vnd.wap.wml" => "mime-html",
+		"text/x-csh" => "mime-shellscript",
+		"text/x-python" => "mime-shellscript",
+		"text/x-sh" => "mime-shellscript",
+		"text/x-vcalendar" => "mime-vcalendar",
+		"text/x-vcard" => "mime-address-book",
+		"text/x-zsh" => "mime-shellscript",
+		"text/x-makefile" => "mime-make",
 		"video" => "video-x-generic",
 		"x-font-afm" => "font-x-generic");
+}
 
+function getIconMimeFile($sysmime) {
+  $icon="";
+  if ($sysmime) {    
+    $tmime=explode(";",$sysmime);
+    $mime=trim($tmime[0]);
+    $mimeIcon=getIconMimeArray();
+    if (isset($mimeIcon[$mime])) {
+      $icon=$mimeIcon[$mime].".png";	  
+    } else {
+      $p=strpos($mime, '/');
+      $mime=substr($mime,0,$p);
+      if (isset($mimeIcon[$mime])) {
+      $icon=$mimeIcon[$mime].".png";
+      } 
+    }
+  }
+  return $icon;
+}
 
+function getSysMimeFile($f,$fn="") {
+  $sys = trim(`file -bi "$f"`);
+  $txt=getTextMimeFile($f);
+
+  // correct errors of file function
+  if (preg_match('/Makefile/',$fn)) return 'text/x-makefile';
+  if (preg_match('/ASCII C\+\+/',$txt)) {
+     if (preg_match('/\.h$/',$fn))    return 'text/x-c-header';
+     return 'text/x-c++';
+  } 
+  if (preg_match('/PHP script/',$txt))  return 'text/x-php';
+  
+  // open office archive
+  if (preg_match('/zip$/',$sys)) {
+  
+    if (preg_match('/OpenOffice/',$txt)) {
+      if (preg_match('/\.sxw$/',$fn))    return 'application/vnd.sun.xml.writer';
+      if (preg_match('/\.sxc$/',$fn))    return 'application/vnd.sun.xml.calc';
+      if (preg_match('/\.sxi$/',$fn))    return 'application/vnd.sun.xml.impress';
+      if (preg_match('/\.sxd$/',$fn))    return 'application/vnd.sun.xml.draw';
+      return 'application/vnd.sun.xml.writer';
+      
+  }
+  if (preg_match('/OpenDocument/',$txt)) {
+      if (preg_match('/\.odp$/',$fn))    return 'application/vnd.oasis.opendocument.presentation';
+      if (preg_match('/\.odt$/',$fn))    return 'application/vnd.oasis.opendocument.text';
+      if (preg_match('/\.ods$/',$fn))    return 'application/vnd.oasis.opendocument.spreadsheet';
+      if (preg_match('/\.odg$/',$fn))    return 'application/vnd.oasis.opendocument.drawing';
+     return 'application/vnd.oasis.opendocument.text';
+    
+  }
+  }
+  if (!$sys ) {
+    // try with text only
+    if ($txt) {
+      if (preg_match('/^XML/',$txt))  return 'text/xml';
+    }
+  }
+
+  return $sys;
+}
+function getTextMimeFile($f) {
+  $txt = trim(`file -b "$f"`);
+
+  if (! $txt) return " ";
+  return $txt;
+}
 ?>
--- Class/Method.SimpleFile.php
+++ Class/Method.SimpleFile.php
@@ -1,49 +1,45 @@
 <?php
 
-
+function postModify() {
+  $this->computeMime();
+}
+function specRefresh() {
+  $this->computeMime();
+}
   /**
    * compute the mime type and the size
    */
-function postModify() {
+function computeMime() {
   $f=$this->getValue("sfi_file");
   if ($f) {
     if (ereg ("(.*)\|(.*)", $f, $reg)) {
       $vf = newFreeVaultFile($this->dbaccess);
       if ($vf->Show($reg[2], $info) == "") {
 
+	include_once ("WORKSPACE/Lib.FileMime.php");
 
-	$this->setValue("sfi_mimetxt",trim(`file -b "$info->path"`));
-	$this->setValue("sfi_mimesys",trim(`file -bi "$info->path"`));
+	$this->setValue("sfi_mimetxt",getTextMimeFile($info->path));
+	$this->setValue("sfi_mimesys",getSysMimeFile($info->path,$info->name));
 	$this->setValue("sfi_title",$info->name);
 	$this->setValue("sfi_filesize",$info->size);
 
-	include ("WORKSPACE/Lib.FileMime.php");
 	
 	$mime=$this->getValue("sfi_mimesys");
-	if ($mime) {
-	  $tmime=explode(",; ",$mime);
-	  $mime=trim($tmime[0]);
-	  if (isset($mimeIcon[$mime])) {
-	    $this->setValue("sfi_mimeicon",$mimeIcon[$mime].".png");
-	    $this->icon=$this->getValue("sfi_mimeicon");	  
-	  } else {
-	    $p=strpos($mime, '/');
-	    $mime=substr($mime,0,$p);
-	    if (isset($mimeIcon[$mime])) {
-	      $this->setValue("sfi_mimeicon",$mimeIcon[$mime].".png");
-	      $this->icon=$this->getValue("sfi_mimeicon");
-	    } 
-	  }
+	$icon=getIconMimeFile($mime);
+	if ($icon) {	  
+	      $this->setValue("sfi_mimeicon",$icon);
+	      $this->icon=$icon;
+	} else {
+	  $fdoc=$this->getFamDoc();
+	  $this->icon=$fdoc->icon;
 	}
+
+
       }
     }
   }
+
 }
 
-function getFileMimeType($f) {
-  if ($f) {
-    
-  }
-}
 
 ?>
