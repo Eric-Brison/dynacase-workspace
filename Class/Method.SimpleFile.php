ca2262f6b80c2fe51406944db5cdc42c3cfe8e33
eric <eric>
2006-04-28 14:34:58 +0000

    begin spec view for simple file

--- Class/Method.SimpleFile.php
+++ Class/Method.SimpleFile.php
@@ -1,5 +1,8 @@
 <?php
 
+var $defaultview= "WORKSPACE:VIEWSIMPLEFILE:T";
+//var $defaultedit= "WORKSPACE:EDITSIMPLEFILE:T";
+
 function postModify() {
   $this->computeMime();
 }
@@ -38,6 +41,27 @@ function computeMime() {
       }
     }
   }
+}
+
+
+function viewsimplefile($target="_self",$ulink=true,$abstract=false) {
+  $this->viewdefaultcard($target,$ulink,$abstract);
+
+  if ($this->revision == 0) {
+    $cdate=FrenchDateToUnixTs($this->cdate);
+  } else {
+    $idoc=new_doc($this->dbaccess,$this->initid);
+    $cdate=FrenchDateToUnixTs($idoc->cdate);    
+  }
+  $this->lay->set("createdate",strftime("%A %d %B %Y",$cdate));
+  $this->lay->set("moddate",strftime("%A %d %B %Y",$this->revdate));
+
+  $size=$this->getValue("sfi_filesize");
+  if ($size < 0) $dsize="";
+  else if ($size < 1024) $dsize=sprintf(_("%d bytes"),$size);
+  else if ($size < 1048576) $dsize=sprintf(_("%d kb"),$size/1024);
+  else $dsize=sprintf(_("%.01f Mb"),$size/1048576);
+  $this->lay->set("dsize",$dsize);
 
 }
 
