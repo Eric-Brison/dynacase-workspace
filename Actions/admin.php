bc53339ce45d1eb41c4f9585583b438e8ec2ad24
eric <eric>
2006-12-04 11:32:28 +0000

    view icon when list workspaces

--- Actions/admin.php
+++ Actions/admin.php
@@ -3,7 +3,7 @@
  * Display doucment explorer
  *
  * @author Anakeen 2006
- * @version $Id: admin.php,v 1.1 2006/07/21 15:27:41 eric Exp $
+ * @version $Id: admin.php,v 1.2 2006/12/04 11:32:28 eric Exp $
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @package FREEDOM
  * @subpackage 
@@ -30,7 +30,10 @@ function admin(&$action) {
 
   $filter=array();
   $ls = getChildDoc($dbaccess, 0 ,0,"ALL", $filter, $action->user->id, "TABLE","WORKSPACE");
-  
+  foreach ($ls as $k=>$v) {
+    $ls[$k]["ICON"]=$fdoc->getIcon($v["icon"]);
+  }
+
   $action->lay->setBlockData("SPACES",$ls);
   $action->lay->set("ficon",$fdoc->geticon());
 
