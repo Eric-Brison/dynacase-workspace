c7157c307ad9302e6182ecccc8638760849f3cd5
eric <eric>
2007-03-12 17:24:28 +0000

    new init toolbar for fckeditor

---
+++ Zone/ws_fckeditor.js
@@ -0,0 +1,16 @@
+FCKConfig.ToolbarSets["ws"] = [
+        ['Source','DocProps','-','Save','NewPage','Preview','-','Templates'],
+        ['Cut','Copy','Paste','PasteText','PasteWord','-','Print','SpellCheck'],
+
+        ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+        ['-','Table','Rule','Smiley','SpecialChar','PageBreak'],
+        '/',
+        ['Bold','Italic','Underline','StrikeThrough','-','Subscript','Superscript'],
+        ['OrderedList','UnorderedList','-','Outdent','Indent'],
+        ['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],
+        ['Link','Unlink','Anchor'],
+        '/',
+        ['FontFormat','FontSize'],
+        ['TextColor','BGColor'],
+        ['FitWindow','-','About']
+] ;
